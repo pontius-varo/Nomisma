2c4189ae08ee046395591d08f4a915a52dfedbe1
Robert Tu <thrtu88@gmail.com>
2021-05-04 18:41:51 -0700

    update transfer function

--- Nomisma.sol
+++ Nomisma.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.8.0:
+pragma solidity ^0.8.0;
 // "Nomisma" is the greek term for currency. This is serving as a placeholder for now
 // as we haven't decided on a name
 
@@ -6,10 +6,10 @@ pragma solidity ^0.8.0:
 
 contract Nomisma {
   // Some variables
-  string public constant name;
-  string public constant symbol;
-  uint8 public constant decimals;
-
+  string public name;
+  string public symbol;
+  uint8 public decimals;
+  uint256 public _totalSupply = 9001;
 
   // Mapping objects
   mapping(address => uint256) balances;
@@ -18,6 +18,7 @@ contract Nomisma {
   // skeleton functions that must be filled in
 
   function totalSupply() public view returns (uint256){
+    return _totalSupply;
   }
 
   function balanceOf(address tokenOwner) public view returns(uint){
@@ -29,7 +30,20 @@ contract Nomisma {
   }
 
   function transfer(address _to, uint _tokens) public returns (bool){
-  }
+      // transfer amount of _tokens from owner's balances
+      // check that owner's balance >= amount of _tokens to transfer AND _tokens amount > 0
+        if (balances[msg.sender] >= _tokens && _tokens > 0) {
+            // subtract _token amount from owner balance
+            balances[msg.sender] -= _tokens;
+            // add _token amount _to address
+            balances[_to] += _tokens;
+            // send balance
+            emit Transfer(msg.sender, _to, _tokens);
+            return true;
+        } 
+        return false;
+    }      
+      
 
   function approve(address spender, uint _tokens) public returns (bool){
   }
@@ -39,9 +53,8 @@ contract Nomisma {
 
   // Skeleton events neccessary for the contract
   // These events are emitted when a user is granted rights to withdraw and transfer
-  event Approval (address indexed tokenOwner, address indexed spender, uint tokens){
-  }
+  event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
-  event Transfer_Event(address indexed from, address indexed to, uint _tokens){
-  }
+  event Transfer(address indexed from, address indexed to, uint _tokens);
+  
 }
